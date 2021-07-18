31efeb2274e8b84219d526c0f3a1e266796480f2
NinaNekonoran <franzese.rozen@gmail.com>
2021-07-18 11:34:05 +0200

    Setting title and meta tags for Angular SEO.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Title} from '@angular/platform-browser';
 import {ActivatedRoute, Router} from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +8,23 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-
-
   redirection: string = null;
-  public constructor(private titleService: Title, private route: ActivatedRoute, private router: Router) {
-  }
-  ngOnInit(): void {
+
+  constructor(private titleService: Title, private route: ActivatedRoute, private router: Router, private meta: Meta) {
+    this.meta.addTags([
+      {name: 'description', content: 'Découvrez nos quelques recettes simples, familiales, classiques, et traditionnelles. Nous vous souhaitons un bon appétit.'},
+      {name: 'author', content: 'Mamie Cameroun et les jumeaux'},
+      {name: 'keywords', content: 'Angular, Cuisine, Recette, Cameroun, Afrique'}
+    ]);
     this.titleService.setTitle('Mamie Cameroun');
+  }
+
+  ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.redirection = params.redirection;
       if (this.redirection) {
         this.router.navigate([
-            'recipe/' + this.redirection + '/'
+          'recipe/' + this.redirection + '/'
         ]);
       }
     });
