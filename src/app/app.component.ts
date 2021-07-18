8bf493b9d7e0675dd16eee8dab646513758a35a9
NinaNekonoran <franzese.rozen@gmail.com>
2021-07-18 11:21:19 +0200

    Setting title and meta tags for Angular SEO.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Title} from '@angular/platform-browser';
 import {ActivatedRoute, Router} from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,15 @@ export class AppComponent implements OnInit{
 
 
   redirection: string = null;
-  public constructor(private titleService: Title, private route: ActivatedRoute, private router: Router) {
+  constructor(private titleService: Title, private route: ActivatedRoute, private router: Router, private meta: Meta) {
+    this.meta.addTags([
+      {name: 'description', content: 'Découvrez nos quelques recettes simples, familiales, classiques, et traditionnelles. Nous vous souhaitons un bon appétit.'},
+      {name: 'author', content: 'Mamie Cameroun et les jumeaux'},
+      {name: 'keywords', content: 'Angular, Cuisine, Recette, Cameroun, Afrique'}
+    ]);
+    this.titleService.setTitle('Mamie Cameroun');
   }
   ngOnInit(): void {
-    this.titleService.setTitle('Mamie Cameroun');
     this.route.queryParams.subscribe(params => {
       this.redirection = params.redirection;
       if (this.redirection) {
